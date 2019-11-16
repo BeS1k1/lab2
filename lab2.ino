d7a6f0c9957964d7d97b41711c2d7b2f0e1fff31
Nikolay <korshunov99@mail.ru>
2019-11-16 13:43:39 +0500

    first ver

---
+++ lab2.ino
@@ -0,0 +1,97 @@
+#include <Servo.h>
+
+Servo bottomServo;
+Servo topServo;
+
+int currentValueOfTop;
+int currentValueOfBottom;
+
+const char keyUp = 'U';
+const char keyDown = 'D';
+const char keyRight = 'R';
+const char keyLeft = 'L';
+const char keyNone = 'N';
+
+const byte rowAmount = 4;
+const byte colAmount = 4;
+
+char keyMatrix[rowAmount][colAmount] = {
+  {keyNone, keyUp,    keyNone,  keyNone},
+  {keyLeft, keyNone,  keyRight, keyNone},
+  {keyNone, keyDown,  keyNone,  keyNone},
+  {keyNone, keyNone,  keyNone,  keyNone}
+};
+
+static bool keyDownMatrix[rowAmount][colAmount];
+
+byte rowPins[rowAmount] = { 5, 4, 3, 2 };
+byte colPins[colAmount] = { 6, 7, 8, 9 };
+
+void setup() {
+  bottomServo.attach(10);
+  topServo.attach(9);
+
+  currentValueOfTop = topServo.read();
+  currentValueOfBottom = bottomServo.read();
+
+  for (int i = 0; i < rowAmount; i++) {
+    pinMode(rowPins[i], OUTPUT);
+    digitalWrite(rowPins[i], HIGH);
+  }
+
+  for (int i = 0; i < colAmount; i++) {
+    pinMode(colPins[i], INPUT);
+    digitalWrite(colPins[i], HIGH);
+  }
+}
+
+void loop() {
+  char currentKey = getKey();
+
+  switch(currentKey){
+    case 'U':
+    {
+      topServo.write(++currentValueOfTop);
+      break;
+    }
+    case 'D':
+    {
+      topServo.write(--currentValueOfTop);
+      break;
+    }
+    case 'R':
+    {
+      bottomServo.write(++currentValueOfBottom);
+      break;
+    }
+    case 'L':
+    {
+      bottomServo.write(--currentValueOfBottom);
+      break;
+    }
+  }
+}
+
+char getKey()
+{
+  char result = keyNone;
+  for (int i = 0; i < rowAmount; i++) {
+    for (int j = 0; j < colAmount; j++) {
+      if (isKeyDown(i, j)) {
+        return keyMatrix[i][j];
+      }
+    } 
+  }
+  return keyNone;
+}
+
+bool isKeyDown(int i, int j)
+{
+  bool result = false;
+  digitalWrite(rowPins[i], LOW);
+  if (digitalRead(colPins[j]) == LOW) {
+    result = true;
+  }
+  digitalWrite(rowPins[i], HIGH);
+  return result;
+}
